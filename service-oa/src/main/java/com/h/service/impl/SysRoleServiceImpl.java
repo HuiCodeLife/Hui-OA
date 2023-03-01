396e8a1dce867e0f8b2bd70e9edc7eabb52187d8
Lin <599775158@qq.com>
2023-03-01 13:37:44 +0800

    整合knife4j

--- service-oa/src/main/java/com/h/service/impl/SysRoleServiceImpl.java
+++ service-oa/src/main/java/com/h/service/impl/SysRoleServiceImpl.java
@@ -4,11 +4,14 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.h.mapper.SysRoleMapper;
 import com.h.model.system.SysRole;
 import com.h.service.SysRoleService;
+import org.springframework.stereotype.Service;
 
 /**
  * @author: Lin
  * @since: 2023-03-01
  */
+
+@Service
 public class SysRoleServiceImpl extends ServiceImpl<SysRoleMapper, SysRole> implements SysRoleService {
 
 
