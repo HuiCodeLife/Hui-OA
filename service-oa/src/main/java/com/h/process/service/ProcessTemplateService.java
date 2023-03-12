fc7cf0e150673efa7f4d8420f0c453e6c89701a9
Lin <599775158@qq.com>
2023-03-12 13:59:12 +0800

    添加审批模板CRUD

--- service-oa/src/main/java/com/h/process/service/ProcessTemplateService.java
+++ service-oa/src/main/java/com/h/process/service/ProcessTemplateService.java
@@ -1,5 +1,7 @@
 package com.h.process.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.h.model.process.ProcessTemplate;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +15,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface ProcessTemplateService extends IService<ProcessTemplate> {
 
+    /**
+     * 查询审批流程模板
+     * @param pageParam page
+     * @return 结果
+     */
+    IPage<ProcessTemplate> selectPage(Page<ProcessTemplate> pageParam);
 }
--- service-oa/src/main/java/com/h/process/service/impl/ProcessTemplateServiceImpl.java
+++ service-oa/src/main/java/com/h/process/service/impl/ProcessTemplateServiceImpl.java
@@ -1,10 +1,19 @@
 package com.h.process.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.h.model.process.ProcessTemplate;
+import com.h.model.process.ProcessType;
 import com.h.process.mapper.ProcessTemplateMapper;
 import com.h.process.service.ProcessTemplateService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.h.process.service.ProcessTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +26,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProcessTemplateServiceImpl extends ServiceImpl<ProcessTemplateMapper, ProcessTemplate> implements ProcessTemplateService {
 
+
+    @Autowired
+    private ProcessTypeService processTypeService;
+
+    @Override
+    public IPage<ProcessTemplate> selectPage(Page<ProcessTemplate> pageParam) {
+        // 查询分页数据 根据更新时间降序
+        Page<ProcessTemplate> processTemplatePage =
+                baseMapper.selectPage(pageParam, new LambdaQueryWrapper<ProcessTemplate>().orderByDesc(ProcessTemplate::getUpdateTime));
+        // 获取processTemplate列表数据
+        List<ProcessTemplate> records = processTemplatePage.getRecords();
+        // 封装processTypeName
+        records.forEach(processTemplate -> {
+            // 获取processType的id
+            Long processTypeId = processTemplate.getProcessTypeId();
+            // 根据processType的id获取processTypeName
+            ProcessType processType = processTypeService.getOne(new LambdaQueryWrapper<ProcessType>().eq(ProcessType::getId, processTypeId));
+            if (processType != null) {
+                String processTypeName = processType.getName();
+                // 封装processTypeName属性
+                processTemplate.setProcessTypeName(processTypeName);
+            }
+        });
+        return processTemplatePage;
+    }
 }
